08870ebc60c0d9d1d9411a51ecbb213987fc29e6
Nunoki <79094039+Nunoki@users.noreply.github.com>
2021-10-01 00:36:42 +0200

    transparent background on html element

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,12 +1,15 @@
 (function(){
     let style = document.createElement("style");
     style.innerHTML = `
+        html {
+            background: transparent !important;
+        }
         body {
             background: black !important;
-            filter: invert() hue-rotate(180deg);
+            filter: invert() hue-rotate(180deg) !important;
         }
         img, video, iframe {
-            filter: invert() hue-rotate(180deg);
+            filter: invert() hue-rotate(180deg) !important;
         }
     `;
     document.head.append(style);
